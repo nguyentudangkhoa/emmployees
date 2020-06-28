75529ee3cfc028539074db34c7589560ce36f2ac
Khoa <nguyentudangkhoa@gmail.com>
2020-06-28 12:52:34 +0700

    update database

--- employees.sql
+++ employees.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 17, 2020 at 04:30 PM
+-- Generation Time: Jun 28, 2020 at 07:21 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.5
 
@@ -64,6 +64,66 @@ CREATE TABLE `failed_jobs` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `house`
+--
+
+CREATE TABLE `house` (
+  `id` int(11) NOT NULL,
+  `house_name` varchar(300) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL,
+  `house_type` varchar(50) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL,
+  `house_details` varchar(300) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL,
+  `house_address` varchar(50) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL,
+  `id_Location` int(11) DEFAULT NULL,
+  `house_image` varchar(225) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+--
+-- Dumping data for table `house`
+--
+
+INSERT INTO `house` (`id`, `house_name`, `house_type`, `house_details`, `house_address`, `id_Location`, `house_image`, `created_at`, `updated_at`) VALUES
+(33, 'Phòng trọ 30m2, full tiện nghi, an ninh trung tâm Tô Hiến Thành', 'Phòng trọ', '30m2, 2 toilet', 'Tô Hiến Thành', 10, 'house2.jpg', '2020-05-19 06:45:27', '2020-05-20 06:47:32'),
+(34, 'Phòng trọ 30m2, full tiện nghi, an ninh trung tâm Tô Hiến Thành', 'Phòng trọ', '30m2, 2 toilet', 'Tô Hiến Thành', 11, 'house2.jpg', '2020-05-20 02:03:07', '2020-05-20 02:03:07'),
+(39, 'Nhà ở 30m2, full tiện nghi, an ninh trung tâm Tô Hiến Thành', 'Nhà ở', '30m2, 2 toilet', 'Tô Hiến Thành', 16, 'house2.jpg', '2020-05-20 03:17:47', '2020-05-20 03:17:47'),
+(40, 'Phòng trọ 30m2, full tiện nghi, an ninh trung tâm Tô Hiến Thành', 'Phòng trọ', '30m2, 2 toilet', 'Tô Hiến Thành', 12, 'house2.jpg', '2020-05-20 03:18:08', '2020-05-20 03:18:08'),
+(41, 'Phòng trọ 30m2, full tiện nghi, an ninh trung tâm Tô Hiến Thành', 'Phòng trọ', '30m2, 2 toilet', 'Tô Hiến Thành', 12, 'house.jpg', '2020-05-20 03:18:23', '2020-05-20 03:18:23'),
+(42, 'Phòng trọ 30m2, full tiện nghi, an ninh trung tâm Tô Hiến Thành', 'Phòng trọ', '30m2, 2 toilet', 'Tô Hiến Thành', 10, 'house.jpg', '2020-05-20 03:18:23', '2020-05-20 04:54:55'),
+(43, 'Nhà ở 30m2, full tiện nghi, an ninh trung tâm Tô Hiến Thành', 'Nhà ở', '30m2, 2 toilet', 'Tô Hiến Thành', 10, 'house2.jpg', '2020-05-20 03:18:50', '2020-05-20 07:16:12'),
+(44, 'Nhà ở 30m2, full tiện nghi, an ninh trung tâm Tô Hiến Thành', 'Nhà ở', '30m2, 2 toilet', 'Tô Hiến Thành', 18, 'house.jpg', '2020-05-20 03:19:09', '2020-05-20 03:19:09'),
+(45, 'Nhà trọ 30m2, full tiện nghi, an ninh trung tâm Tô Hiến Thành', 'Nhà trọ', '30m2, 2 toilet', '123 Trần Hưng Đạo', 10, 'photo-1512917774080-9991f1c4c750.jpg', '2020-06-24 11:10:50', '2020-06-24 11:11:45');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location`
+--
+
+CREATE TABLE `location` (
+  `id` int(11) NOT NULL,
+  `location_name` varchar(100) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT current_timestamp(),
+  `created_at` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+--
+-- Dumping data for table `location`
+--
+
+INSERT INTO `location` (`id`, `location_name`, `parent_id`, `updated_at`, `created_at`) VALUES
+(10, 'Phường 10, Quận 8', 2, '2020-05-19 02:49:02', '2020-05-19 02:48:59'),
+(11, 'Phường 11, Quận 8', 2, '2020-05-19 02:48:56', '2020-05-19 02:48:52'),
+(12, 'Phường 12, Quận 8', 2, '2020-05-19 02:48:45', '2020-05-19 02:48:37'),
+(13, 'Phường 3, Quận 8', 2, '2020-05-19 02:46:17', '2020-05-19 02:46:17'),
+(15, 'Phường 3, Quận 9', 2, '2020-05-19 02:47:02', '2020-05-19 02:47:02'),
+(16, 'Phường 4, Quận 8', 2, '2020-05-19 04:05:28', '2020-05-19 04:05:28'),
+(18, 'Phường 4, Quận 9', 2, '2020-05-19 04:25:12', '2020-05-19 04:25:12');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `migrations`
 --
 
@@ -93,7 +153,7 @@ CREATE TABLE `overtime` (
   `user_id` bigint(20) UNSIGNED NOT NULL,
   `date_ot` date NOT NULL,
   `start_time` time NOT NULL,
-  `end_time` int(11) NOT NULL,
+  `end_time` time NOT NULL,
   `place_ot` varchar(100) COLLATE utf8mb4_vietnamese_ci NOT NULL,
   `task_name` varchar(200) COLLATE utf8mb4_vietnamese_ci NOT NULL,
   `note` varchar(200) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL,
@@ -101,6 +161,13 @@ CREATE TABLE `overtime` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+--
+-- Dumping data for table `overtime`
+--
+
+INSERT INTO `overtime` (`id`, `user_id`, `date_ot`, `start_time`, `end_time`, `place_ot`, `task_name`, `note`, `status`, `created_at`, `updated_at`) VALUES
+(1, 1, '2020-06-26', '18:30:00', '21:34:00', 'Back end', 'Login Ajax and verified', 'deadline', NULL, '2020-06-26 14:36:07', '2020-06-26 14:36:07');
 
 -- --------------------------------------------------------
 
@@ -142,6 +209,8 @@ CREATE TABLE `users` (
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `salary` int(12) DEFAULT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `login_at` datetime DEFAULT NULL,
+  `logout_at` datetime DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -150,10 +219,10 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `address`, `identity_card`, `issue_place`, `issue_date`, `university`, `granduate_year`, `start_job_at`, `birthday`, `avatar`, `note`, `role`, `position`, `gender`, `total_holidays`, `email_verified_at`, `password`, `salary`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Nguyen Tu Dang Khoa', 'nguyentudangkhoa@gmail.com', 'Hồ Chí Minh', '261530344', 'Bình Thuận', '2017-07-28', 'Nguyễn Tất Thành', 1910, '2020-06-01 15:55:00', '1997-06-03', 'avatar04.png', 'Nhân Viên', 'user', 'Software Engineer', 'Male', 6, NULL, '$2y$10$BSxGHqSOOUTIhsIX3LIjvOk1fPRhRSfwrp6FZu5PHbcXGnxiFadr6', 7000000, 'RmYnghHGpoxzgS4CEhirdWNNBWBeJ5TyJtLGzUL2OsmcIZnJXw6RPSemkPVR', '2020-05-28 01:39:28', '2020-06-13 04:14:33'),
-(2, 'Nguyen Tu Dang Khoa', 'khoakute1997@gmail.com', 'Hồ Chí Minh', '123123123123', 'Công An Tp. Hồ Chí Minh', '2018-02-14', 'Nguyễn Tất Thành', 2020, '2020-06-01 17:10:00', '1997-06-03', 'avatar.png', 'Nhân viên chính thức', 'user', 'QC-tester', 'Male', 14, NULL, '$2y$10$eXZDGfsvJEOHBrcQdSVWr.Qn/toanzN5TZP688y.DN1HTKRrKsxcO', 12000000, 'URmBn7xL4BWThfNW7mEf5S5zoSHARQA3SFwzfSmfJJmnMvbKyDBSUUvHkh2u', '2020-05-29 23:03:19', '2020-06-12 10:05:08'),
-(3, 'admin', 'admin@gmail.com', 'Hồ Chí Minh', '123123123123', 'Công An Tp. Hồ Chí Minh', '2020-06-02', 'Tôn Đức Thắng', 1910, '2020-06-12 08:55:00', '1994-06-12', NULL, 'admin hệ thống', 'admin', NULL, 'Male', 14, NULL, '$2y$10$w.lF9tS4AzNbECrpD6UB9uC33t1FxPaXl7S3W9Swg07aSIl9hXOp6', NULL, 'YeXlwyKQmaoZ0PgvXhkOJlHOq36sCXb1s8ai7GYZVVvLHjnLHPUD60r8x6Cb', '2020-06-01 06:32:34', '2020-06-12 01:55:37');
+INSERT INTO `users` (`id`, `name`, `email`, `address`, `identity_card`, `issue_place`, `issue_date`, `university`, `granduate_year`, `start_job_at`, `birthday`, `avatar`, `note`, `role`, `position`, `gender`, `total_holidays`, `email_verified_at`, `password`, `salary`, `remember_token`, `login_at`, `logout_at`, `created_at`, `updated_at`) VALUES
+(1, 'Nguyen Tu Dang Khoa', 'nguyentudangkhoa@gmail.com', 'Hồ Chí Minh', '261530344', 'Bình Thuận', '2017-07-28', 'Nguyễn Tất Thành', 1910, '2020-06-01 15:55:00', '1997-06-03', 'avatar.png', 'Nhân Viên', 'user', 'Software Engineer', 'Male', 6, NULL, '$2y$10$BSxGHqSOOUTIhsIX3LIjvOk1fPRhRSfwrp6FZu5PHbcXGnxiFadr6', 7000000, 'EdToweIOb6Zkilr2lR2kcCHJNcVVAzzcv6dziydYDxGRfzIQiSrTWW73ymFt', '2020-06-26 21:45:28', '2020-06-21 12:51:40', '2020-05-28 01:39:28', '2020-06-26 15:16:03'),
+(2, 'Nguyen Tu Dang Khoa', 'khoakute1997@gmail.com', 'Hồ Chí Minh', '123123123123', 'Công An Tp. Hồ Chí Minh', '2018-02-14', 'Nguyễn Tất Thành', 2020, '2020-06-01 17:10:00', '1997-06-03', 'avatar.png', 'Nhân viên chính thức', 'user', 'QC-tester', 'Male', 14, NULL, '$2y$10$eXZDGfsvJEOHBrcQdSVWr.Qn/toanzN5TZP688y.DN1HTKRrKsxcO', 12000000, 'URmBn7xL4BWThfNW7mEf5S5zoSHARQA3SFwzfSmfJJmnMvbKyDBSUUvHkh2u', NULL, NULL, '2020-05-29 23:03:19', '2020-06-12 10:05:08'),
+(3, 'admin', 'admin@gmail.com', 'Hồ Chí Minh', '123123123123', 'Công An Tp. Hồ Chí Minh', '2020-06-02', 'Tôn Đức Thắng', 1910, '2020-06-12 08:55:00', '1994-06-12', NULL, 'admin hệ thống', 'admin', NULL, 'Male', 14, NULL, '$2y$10$w.lF9tS4AzNbECrpD6UB9uC33t1FxPaXl7S3W9Swg07aSIl9hXOp6', NULL, 'brFNcq2qFQzQvCpGlF4GWb2X1xD5avXxoh60A6eUQeIDBBtGRNBJOoOyVYmq', '2020-06-25 18:42:23', '2020-06-26 21:45:18', '2020-06-01 06:32:34', '2020-06-26 14:45:18');
 
 --
 -- Indexes for dumped tables
@@ -170,6 +239,19 @@ ALTER TABLE `absence_letter`
 -- Indexes for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `house`
+--
+ALTER TABLE `house`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_House_Location` (`id_Location`);
+
+--
+-- Indexes for table `location`
+--
+ALTER TABLE `location`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -215,6 +297,18 @@ ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `house`
+--
+ALTER TABLE `house`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+
+--
+-- AUTO_INCREMENT for table `location`
+--
+ALTER TABLE `location`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
@@ -224,7 +318,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `overtime`
 --
 ALTER TABLE `overtime`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -241,6 +335,12 @@ ALTER TABLE `users`
 --
 ALTER TABLE `absence_letter`
   ADD CONSTRAINT `fk_adsence_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `house`
+--
+ALTER TABLE `house`
+  ADD CONSTRAINT `FK_House_Location` FOREIGN KEY (`id_Location`) REFERENCES `location` (`id`);
 
 --
 -- Constraints for table `overtime`
